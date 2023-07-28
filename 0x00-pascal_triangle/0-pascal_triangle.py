9b18bd4b730ca4b2d8c641cca3b1db65d64e8154
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-07-28 09:21:06 +0000

    Fixed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,11 +13,21 @@ def pascal_triangle(n):
     representing the Pascal's triangle of n."""
     if n <= 0:
         return []
+    #triangle = [[1]]
+    #for i in range(1, n + 1):
+    #    row = [1]
+    #    for j in range(1, i):
+    #        row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+    #    row.append(1)
+    #    triangle.append(row)
+    #return triangle
     triangle = [[1]]
     for i in range(1, n + 1):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-    return triangle
+      row = [1] * i
+      for j in range(i):
+        if j == 0 or j == i - 1:
+          row[j] = 1
+        else:
+          row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+      triangle.append(row)
+    return triangle[1:]
