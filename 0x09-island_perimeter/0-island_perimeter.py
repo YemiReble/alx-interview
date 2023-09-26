9b7a2c8d945a52472f7781839b93d2435babef16
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-26 14:36:00 +0100

    Island Perimeter Source Code

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """Calculates the perimeter of the island described in grid.
+
+    Args:
+        grid: A list of lists of integers, where 0 represents water and 1 represents land.
+
+    Returns:
+        The perimeter of the island.
+    """
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                # Check if the cell is on the edge of the grid.
+                if i == 0 or i == len(grid) - 1 or j == 0 or j == len(grid[0]) - 1:
+                    perimeter += 4
+                else:
+                    # Check if the cell is adjacent to any water cells.
+                    if grid[i - 1][j] == 0 or grid[i + 1][j] == 0 or grid[i][j - 1] == 0 or grid[i][j + 1] == 0:
+                        perimeter += 1
+
+    return perimeter
