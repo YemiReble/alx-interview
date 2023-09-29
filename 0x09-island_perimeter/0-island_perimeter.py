0fe377581eaa7843d8136448f685096726a98b13
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-29 11:07:21 +0100

    Update: Reduced the long lines

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,7 +8,8 @@ def island_perimeter(grid):
     """Calculates the perimeter of the island described in grid.
 
     Args:
-        grid: A list of lists of integers, where 0 represents water and 1 represents land.
+        grid: A list of lists of integers, where 0 represents
+            water and 1 represents land.
 
     Returns:
         The perimeter of the island.
@@ -19,11 +20,13 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 # Check if the cell is on the edge of the grid.
-                if i == 0 or i == len(grid) - 1 or j == 0 or j == len(grid[0]) - 1:
+                if i == 0 or i == len(grid) - 1 or j == 0 or \
+                        j == len(grid[0]) - 1:
                     perimeter += 4
                 else:
                     # Check if the cell is adjacent to any water cells.
-                    if grid[i - 1][j] == 0 or grid[i + 1][j] == 0 or grid[i][j - 1] == 0 or grid[i][j + 1] == 0:
+                    if grid[i - 1][j] == 0 or grid[i + 1][j] == 0 or \
+                            grid[i][j - 1] == 0 or grid[i][j + 1] == 0:
                         perimeter += 1
 
     return perimeter
