2f7a25770666baf53f59d39b0c35e904ef7addc2
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-10-06 10:39:31 +0100

    More Test Files Added

---
+++ 0x0A-primegame/Test-Files/main_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+isWinner = __import__('0-prime_game').isWinner
+
+nums = [0] * 10000
+
+for i in range(10000):
+    nums[i] = i
+
+print("Winner: {}".format(isWinner(10000, nums)))
+print("Winner: {}".format(isWinner(7, [5, 5, 5, 5, 5, 2, 2, 2, 2, 2])))
